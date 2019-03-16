f4306e879d641d654fdcd0fc1ae16d765a08a286
iamezell <iamezell@gmail.com>
2019-03-16 18:35:50 -0400

    adding stuff

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Header from './components/Home'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Header />
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
